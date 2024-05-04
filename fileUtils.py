fcb23731ce5aa5a5c1b5ebc8d841147c3d92b47a
SirSevrus <sirsevrus@gmail.com>
2024-05-04 16:20:15 +0530

    modified:   fileUtils.py

--- fileUtils.py
+++ fileUtils.py
@@ -47,3 +47,42 @@ def checkFile(path):
     except Exception as e:
         print('An error occured checking existence of file at', str(path), ' : ',  str(e))
         sys.exit()
+
+def readTextFile(path, chars=-1, lines=False, charLines=False):
+    """
+    Read text from a file and return the content based on specified parameters.
+
+    Parameters:
+    path (str): The path to the text file.
+    chars (int, optional): Number of characters to read from the file. Default is -1, meaning the whole file.
+    lines (bool, optional): If True, returns the lines of the file as a list. Default is False.
+    charLines (int, optional): Number of lines to read as a list of characters. Default is False.
+
+    Returns:
+    str or list: Depending on the parameters provided, returns either a string (if chars is specified) or a list of lines from the file.
+
+    Raises:
+    FileNotFoundError: If the specified file path does not exist.
+    """
+
+    try:
+        if lines:
+            with open(path, 'r') as file:
+                data = file.readlines()
+            if charLines:
+                Data = []
+                for i in range(min(charLines, len(data))):
+                    Data.append(data[i])
+                return Data
+            else:
+                return data
+        else:
+            with open(path, 'r') as file:
+                data = file.read(chars)
+                return data
+    except FileNotFoundError:
+        print('Error: File not found at path', path)
+        sys.exit()
+    except Exception as e:
+        print('An error occurred:', str(e))
+        sys.exit()
